f28e4431d592e8d13dfe6dd48fd486cf8d6868d0
kaiyes <kaiyes.ansary@gmail.com>
2020-07-10 10:51:35 +0600

    added navigation

---
+++ src/home.screen.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function HomeScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Home Screen</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
+
+
+
+
