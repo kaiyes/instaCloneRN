1d4e6426910beb35a980512b27292a2a56bdc543
kaiyes <kaiyes.ansary@gmail.com>
2020-07-10 12:31:45 +0600

    added bottom button

--- src/home.screen.js
+++ src/home.screen.js
@@ -1,6 +1,13 @@
 import {StatusBar} from 'expo-status-bar'
 import React from 'react'
-import {StyleSheet, Text, View, SafeAreaView, Image} from 'react-native'
+import {
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	Image,
+	TouchableOpacity,
+} from 'react-native'
 import {
 	widthPercentageToDP as wp,
 	heightPercentageToDP as hp,
@@ -10,27 +17,37 @@ export default function HomeScreen() {
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.topCard}>
-				<Image
-					style={styles.avatar}
-					source={{
-						uri:
-							'https://pickaface.net/gallery/avatar/unr_willsmith_170130_2033_2pb8gtu.png',
-					}}
-				/>
+				<View style={styles.avatarHolder}>
+					<Image
+						style={styles.avatar}
+						source={{
+							uri:
+								'https://pickaface.net/gallery/avatar/unr_willsmith_170130_2033_2pb8gtu.png',
+						}}
+					/>
+				</View>
+
 				<View style={styles.infoCol}>
-					<View style={styles.numberHolder}>
-						<Text style={styles.number}>604</Text>
-						<Text style={styles.subTitle}>posts</Text>
-					</View>
-					<View style={styles.numberHolder}>
-						<Text style={styles.number}>35.8M</Text>
-						<Text style={styles.subTitle}>followers</Text>
+					<View style={styles.topRow}>
+						<View style={styles.numberHolder}>
+							<Text style={styles.number}>604</Text>
+							<Text style={styles.subTitle}>posts</Text>
+						</View>
+
+						<View style={styles.numberHolder}>
+							<Text style={styles.number}>35.8M</Text>
+							<Text style={styles.subTitle}>followers</Text>
+						</View>
+
+						<View style={styles.numberHolder}>
+							<Text style={styles.number}>152</Text>
+							<Text style={styles.subTitle}>following</Text>
+						</View>
 					</View>
 
-					<View style={styles.numberHolder}>
-						<Text style={styles.number}>152</Text>
-						<Text style={styles.subTitle}>following</Text>
-					</View>
+					<TouchableOpacity style={styles.button}>
+						<Text style={styles.buttonText}>Message</Text>
+					</TouchableOpacity>
 				</View>
 			</View>
 		</SafeAreaView>
@@ -47,20 +64,24 @@ const styles = StyleSheet.create({
 		paddingLeft: wp('3%'),
 		flexDirection: 'row',
 	},
+	avatarHolder: {
+		width: wp('29%'),
+	},
 	avatar: {
-		width: wp('20%'),
-		height: wp('20%'),
-		borderRadius: wp('10%'),
+		width: wp('25%'),
+		height: wp('25%'),
+		borderRadius: wp('12.5%'),
 		borderWidth: 2,
 		borderColor: 'magenta',
 	},
 	infoCol: {
+		marginTop: hp('1%'),
 		flex: 1,
+	},
+	topRow: {
 		flexDirection: 'row',
 		justifyContent: 'space-around',
-		marginTop: hp('1%'),
 	},
-	numberHolder: {},
 	number: {
 		fontWeight: '500',
 		fontSize: 20,
@@ -68,5 +89,17 @@ const styles = StyleSheet.create({
 	},
 	subTitle: {
 		color: 'gray',
+	},
+	button: {
+		borderColor: 'silver',
+		borderWidth: 1,
+		borderRadius: 5,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: hp('1.5%'),
+	},
+	buttonText: {
+		color: '#162129',
+		paddingVertical: hp('1%'),
 	},
 })
